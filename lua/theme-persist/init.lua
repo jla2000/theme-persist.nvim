499a8daf338b0c9e222b938b7546109d0aa62656
Jan Lafferton <jan.lafferton@vector.com>
2024-08-29 14:45:28 +0000

    remove logging

--- lua/theme-persist/init.lua
+++ lua/theme-persist/init.lua
@@ -6,26 +6,20 @@ local function get_colorscheme_file()
 end
 
 M.load = function()
-	vim.notify("Loading colorscheme")
 	local file = io.open(get_colorscheme_file(), "r")
 	if file then
 		local colorscheme = file:read("*a")
 		file:close()
 
 		vim.cmd.colorscheme(colorscheme)
-	else
-		vim.notify("Failed to load file")
 	end
 end
 
 M.save = function(colorscheme)
-	vim.notify("Saving colorscheme")
 	local file = io.open(get_colorscheme_file(), "w")
 	if file then
 		file:write(colorscheme)
 		file:close()
-	else
-		vim.notify("Failed to write file")
 	end
 end
 
